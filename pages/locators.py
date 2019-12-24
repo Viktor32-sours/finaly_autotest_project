af70ac513036b5a5255fcb926baf0a5ae2c1078a
Viktor32 <viktor-k32@yandex.ru>
2019-12-24 19:27:42 +0400

    Changed also added test_product_page

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,18 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class  LoginPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    REGISTER_FORM_LINK = (By.ID, "register_form")
+    LOGIN_FORM_LINK = (By.ID, "login_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_LINK = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    BASKET_VIEW_LINK = (By.CSS_SELECTOR, "")
+    NAME_PRODUCT_LINK = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    NAME_PRODUCT_ON_BASKET_LINK = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+
+    PRICE_PRODUCT_LINK = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    PRICE_PRODUCT_ON_BASKET_LINK = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+
+
 
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,9 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -9,12 +11,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url,  "URL Login link is not presented"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_LINK), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert  self.is_element_present(*LoginPageLocators.REGISTER_FORM_LINK), "Register form is not presented"
