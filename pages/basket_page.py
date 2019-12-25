f163e9ef8f3ebac3f296a78f8b4325b2d5b8b028
Viktor32 <viktor-k32@yandex.ru>
2019-12-25 18:52:18 +0400

    added test from baset page

---
+++ pages/basket_page.py
@@ -0,0 +1,27 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+class BasketPage(BasePage):
+
+
+    def should_basket_page(self):
+        self.should_button_basket()
+        self.should_basket_is_empty() 
+        self.should_in_basket_not_item()
+
+    def go_to_basket(self):
+        button = self.browser.find_element(*BasketPageLocators.BASKET_BUTTOM_LINK)
+        button.click()
+
+    def should_basket_is_empty(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_LINK), "Basket is not empty"
+        
+    def should_button_basket(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_BUTTOM_LINK), "Button 'View basket' is not presented"
+
+    def should_in_basket_not_item(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEM_LINK), "Button 'View basket' is not presented"
+
+   
+
+
